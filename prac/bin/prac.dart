5b9dd878ff5089325d8e84b42a5be3524896d9c1
Чарынчык Айдан Валерьевич <117069700+Aydan-Charynchyk-621@users.noreply.github.com>
2022-11-12 01:54:00 +0700

    Update prac.dart

--- prac/bin/prac.dart
+++ prac/bin/prac.dart
@@ -1,5 +1,55 @@
-import 'package:prac/prac.dart' as prac;
+import 'dart:io';
+import 'dart:math';
 
-void main(List<String> arguments) {
-  print('Hello world: ${prac.calculate()}!');
-}
+void main() {
+
+//Задание 1
+//
+//int n = 21;
+//int proizv = 1;
+//print(n);
+//for (int i = 3; i <= n; i += 3) {
+//proizv *= i;}
+//print("Произведение-$proizv");
+//}
+//
+//
+//Задание 2
+//
+//int sum = 0;
+//var nums = File ('C:/Users/Aydan/Desktop/numsTask2.txt.txt');
+//var ay = nums.readAsStringSync();
+//for (int i = 0; i <= ay.length; i++) {
+//if (i > 0) {
+//sum += i;
+//}
+//print(sum);
+//}
+//}
+//
+//
+//Задание 3
+//
+//double otnosh = 0;
+//var nums = File ('C:/Users/Aydan/Desktop/numsTask3.txt.txt');
+//var ay = nums.readAsStringSync();
+///List<String> prac = ay.split(',');
+//int min = int.parse(prac[0]);
+//int max = int.parse(prac[0]);
+//for (int i = 0; i < prac.length; i++) {
+//if (int.parse(prac[i]) < min && int.parse(prac[i]) != 0)
+//{
+//min = int.parse(prac[i]);
+//}
+//}
+//for (int i = 0; i < prac.length; i++) {
+//if (int.parse(prac[i]) > max)
+//{
+//max = int.parse(prac[i]);
+//}
+//}
+///otnosh = min / max;
+//print(min);
+//print(max);
+//print(otnosh);
+//}
